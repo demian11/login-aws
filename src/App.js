d88ed288963eda4d4225c348bf7d06ee627cef3c
damian <demian1155@outlook.com>
2023-11-11 16:13:53 -0600

    avance2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {BrowserRouter, NavLink, Route, Switch, Routes} from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Switch, Routes } from "react-router-dom";
+
 import Home from "./Home"
 import Register from "./Register"
 import Login from "./Login"
@@ -9,62 +10,62 @@ import React, { useState, useEffect } from "react";
 import { getUser, getToken, setUserSession, resetUserSession } from "./service/AuthService";
 import axios from "axios";
 
-const verifyTokenAPIURL ='https://pu0xnsorsj.execute-api.us-west-2.amazonaws.com/prueba/verify';
+const verifyTokenAPIURL = 'https://pu0xnsorsj.execute-api.us-west-2.amazonaws.com/prueba/verify';
 
 function App() {
 
-  const[isAuthenicating, setAuthenicating] = useState(true);
+  const [isAuthenicating, setAuthenicating] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     const token = getToken();
-    if(token === 'undefined' || token === undefined || token === null || !token) {
+    if (token === 'undefined' || token === undefined || token === null || !token) {
       return;
     }
 
-    const requestConfig ={
-      headers:{
-      'x-api-key': 'teunRcvUeK2ejZP5OaRMH4nfOpNTHKwL5GYNwM2S'
+    const requestConfig = {
+      headers: {
+        'x-api-key': 'teunRcvUeK2ejZP5OaRMH4nfOpNTHKwL5GYNwM2S'
+      }
     }
-  }
-  const requestBody ={
-    user: getUser(),
-    token: token
-  }
+    const requestBody = {
+      user: getUser(),
+      token: token
+    }
 
-  axios.post(verifyTokenAPIURL, requestBody,requestConfig).then (Response =>{
-    setUserSession(Response.data.user, Response.data.token);
-    setAuthenicating(false);
-  }).catch(() => {
-    resetUserSession();
-    setAuthenicating(false);
-  })
-  },[]);
+    axios.post(verifyTokenAPIURL, requestBody, requestConfig).then(Response => {
+      setUserSession(Response.data.user, Response.data.token);
+      setAuthenicating(false);
+    }).catch(() => {
+      resetUserSession();
+      setAuthenicating(false);
+    })
+  }, []);
 
-  const token =getToken();
-  if(isAuthenicating && token) {
+  const token = getToken();
+  if (isAuthenicating && token) {
     return <div className="content">autenticando...</div>
   }
-  
+
 
   return (
-    <div  className="App">
+    <div className="App">
       <BrowserRouter>
-    <div className="header">
-    <NavLink exact activeClassName="active" to="/">Home</NavLink>
-    <NavLink exact activeClassName="active" to="/register">Register</NavLink>
-    <NavLink exact activeClassName="active" to="/login">Login</NavLink>
-    <NavLink exact activeClassName="active" to="/premium-content">Premium Content</NavLink>
-    </div> 
-    <div className="content">
+        <div className="header">
+          <NavLink exact activeClassName="active" to="/">Inicio</NavLink>
+          <NavLink exact activeClassName="active" to="/register">Registro</NavLink>
+          <NavLink exact activeClassName="active" to="/login">Iniciar sesion</NavLink>
+          <NavLink exact activeClassName="active" to="/premium-content">Contenido premium</NavLink>
+        </div>
+        <div className="content">
 
-    <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/register" element={<PublicRoute />} />
-  <Route path="/login" element={<PublicRoute />} />
-  <Route path="/premium-content" element={<PrivateRoute />} />
-</Routes>
-    </div>
-    </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register/" element={<PublicRoute />} />
+            <Route path="/login/" element={<PublicRoute />} />
+            <Route path="/premium-content/" element={<PrivateRoute />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
